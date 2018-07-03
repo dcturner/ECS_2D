0ad6a36b0aa17ee6a4c149b39c3687e6a10e051f
DcTurner <mrdcturner@gmail.com>
2018-07-03 21:58:59 +0100

    matrix transforms (proper) added

--- Assets/src/UI_Core.cs
+++ Assets/src/UI_Core.cs
@@ -15,7 +15,17 @@ public class UI_Core : MonoBehaviour {
         //Draw_ELLIPSE(36, 0.5f, 0.5f, 0.1f, 0.1f, colours);
         //Draw_ELLIPSE_FILL(36, 0.5f, 0.5f, 0.1f, 0.1f, colours);
         //Draw_ARC_FILL(50, 0.5f, 0.5f, 0.25f, 0.75f, 0.2f, 0.4f, Color.cyan);
-        Draw_TRIANGLE_FILL(0.5f, 0.5f, -0.25f, Color.cyan);
+
+        //Draw_TRIANGLE_FILL(0.5f, 0.5f, 0.1f, Color.cyan, Time.realtimeSinceStartup * 100f);
+        int total = 200;
+        float angleDiv = 1f / total;
+        float thickDiv = 0.8f / total;
+        for (int i = 0; i < total; i++)
+        {
+            Draw_ARC_FILL(12, 0.5f, 0.5f, 0f, angleDiv, i*thickDiv, (i+1)*thickDiv, Color.cyan, Time.realtimeSinceStartup * (Mathf.Sin(i*0.01f) * 100f));
+        }
+        //Draw_ARC_FILL(24, 0.5f, 0.5f, 0f, 0.25f, 0.2f, 0.22f, Color.cyan, Time.realtimeSinceStartup * 100f);
+
     }
 
     public static float ScreenX(float _x){
@@ -38,13 +48,23 @@ public class UI_Core : MonoBehaviour {
         return new Vector2(_X, _Y);
     }
 
+    public static void TransformMatrix(float _x, float _y, float _rotation, float _scaleX = 1, float _scaleY = 1){
+        Matrix4x4 m = Matrix4x4.TRS(new Vector3(ScreenX(_x), ScreenY(_y),Z), Quaternion.Euler(0, 0, _rotation), new Vector3(_scaleX, _scaleY, 1));
+        GL.MultMatrix(m);
+    }
+
     public static void Add_VERT(float _x, float _y, Color _col)
     {
         GL.Color(_col);
         GL.Vertex3(ScreenX(_x), ScreenY(_y), Z);
     }
+    public static void Add_VERT_1to1(float _x, float _y, Color _col)
+    {
+        GL.Color(_col);
+        GL.Vertex3(ScreenX(_x), ScreenX(_y), Z);
+    }
 
-    public static void Draw_POLY(Vector3[] _verts, Color[] _colours){
+    public static void Draw_POLY_LINE(Vector3[] _verts, Color[] _colours){
         GL.Begin(GL.LINE_STRIP);
 
         for (int vertIndex = 0; vertIndex < _verts.Length; vertIndex++)
@@ -56,7 +76,7 @@ public class UI_Core : MonoBehaviour {
 
         GL.End();
     }
-    public static void Draw_POLY(Vector3[] _verts, Color _colour)
+    public static void Draw_POLY_LINE(Vector3[] _verts, Color _colour)
     {
         GL.Begin(GL.LINE_STRIP);
 
@@ -69,34 +89,48 @@ public class UI_Core : MonoBehaviour {
         GL.End();
     }
 
-    public static void Draw_TRIANGLE(float _x, float _y, float _size, Color _col){
-        GL.Begin(GL.LINE_STRIP);
-        float _SIZE2 = _size * 0.5f;
-        Add_VERT(_x, _y, _col);
-        Add_VERT(_x -_SIZE2, _y-_size, _col);
-        Add_VERT(_x + _SIZE2, _y-_size, _col);
-        Add_VERT(_x, _y, _col);
-        GL.End();
-    }
-    public static void Draw_TRIANGLE_FILL(float _x, float _y, float _size, Color _col)
+    public static void Draw_TRIANGLE(float _x, float _y, float _size, Color _col, float _rotation = 0)
     {
-        GL.Begin(GL.TRIANGLES);
         float _SIZE2 = _size * 0.5f;
-        Add_VERT(_x, _y, _col);
-        Add_VERT(_x - _SIZE2, _y - _size, _col);
-        Add_VERT(_x + _SIZE2, _y - _size, _col);
+
+        GL.PushMatrix();
+        TransformMatrix(ScreenX(_x), ScreenY(_y), _rotation);
+        GL.Begin(GL.LINE_STRIP);
+
+        Add_VERT(0, 0, _col);
+        Add_VERT(-_SIZE2, -_size, _col);
+        Add_VERT(_SIZE2, -_size, _col);
+        Add_VERT(0, 0, _col);
+
         GL.End();
+        GL.PopMatrix();
+    }
+
+    public static void Draw_TRIANGLE_FILL(float _x, float _y, float _size, Color _col, float _rotation)
+    {
+        float _SIZE2 = _size * 0.5f;
+
+        GL.PushMatrix();
+        TransformMatrix(ScreenX(_x), ScreenY(_y), _rotation);
+        GL.Begin(GL.TRIANGLES);
+
+        Add_VERT(0, 0, _col);
+        Add_VERT(-_SIZE2, -_size, _col);
+        Add_VERT(_SIZE2, -_size, _col);
+
+        GL.End();
+        GL.PopMatrix();
     }
 
     public static void Draw_RECT(float _x, float _y, float _w, float _h, Color _col){
         
         GL.Begin(GL.LINE_STRIP);
 
-        GL.Vertex3(_x, _y, Z);
-        GL.Vertex3(_x + _w, _y, Z);
-        GL.Vertex3(_x + _w, _y + _h, Z);
-        GL.Vertex3(_x, _y + _h, Z);
-        GL.Vertex3(_x, _y, Z);
+        Add_VERT(_x, _y, _col);
+        Add_VERT(_x + _w, _y, _col);
+        Add_VERT(_x + _w, _y + _h, _col);
+        Add_VERT(_x, _y + _h, _col);
+        Add_VERT(_x, _y, _col);
 
         GL.End();
     }
@@ -146,36 +180,40 @@ public class UI_Core : MonoBehaviour {
         }
     }
 
-    public static void Draw_ARC(int _segments, float _x, float _y, float _start, float _end, float _radius_start, float _radius_end, Color _col)
+    public static void Draw_ARC(int _segments, float _x, float _y, float _start, float _end, float _radius_start, float _radius_end, Color _col, float _rotation = 0)
     {
         float _THETA_START = _start * PI2;
         float _THETA_END = _end * PI2;
         float _THETA_RANGE = _THETA_END - _THETA_START;
         float _THETA_DIV = _THETA_RANGE / (_segments-1);
 
+        GL.PushMatrix();
+        TransformMatrix(_x, _y, _rotation);
         GL.Begin(GL.LINE_STRIP);
 
         // BOTTOM arc
         Vector2 _START_VEC = PolarCoord(_THETA_START, _radius_start);
-        Add_VERT(_START_VEC.x + _x, _START_VEC.y + _y, _col);
+        Add_VERT_1to1(_START_VEC.x, _START_VEC.y, _col);
         for (int i = 1; i < _segments; i++)
         {
             Vector2 _V = PolarCoord(_THETA_START + (i*_THETA_DIV), _radius_start);
-            Add_VERT(_V.x + _x, _V.y + _y, _col);
+            Add_VERT_1to1(_V.x, _V.y, _col);
         }
         // TOP arc
         for (int i = 0; i < _segments; i++)
         {
             Vector2 _V = PolarCoord(_THETA_END - (i * _THETA_DIV), _radius_end);
-            Add_VERT(_V.x + _x, _V.y + _y, _col);
+            Add_VERT_1to1(_V.x, _V.y, _col);
         }
 
         // end cap
-        Add_VERT(_START_VEC.x + _x, _START_VEC.y + _y, _col);
+        Add_VERT_1to1(_START_VEC.x, _START_VEC.y, _col);
         GL.End();
+
+        GL.PopMatrix();
     }
 
-    public static void Draw_ARC_FILL(int _segments, float _x, float _y, float _start, float _end, float _radius_start, float _radius_end, Color _col)
+    public static void Draw_ARC_FILL(int _segments, float _x, float _y, float _start, float _end, float _radius_start, float _radius_end, Color _col, float _rotation = 0)
     {
         float _THETA_START = _start * PI2;
         float _THETA_END = _end * PI2;
@@ -197,7 +235,8 @@ public class UI_Core : MonoBehaviour {
 
 
         }
-
+        GL.PushMatrix();
+        TransformMatrix(_x, _y, _rotation);
         for (int i = 0; i < _TOTAL_SEGMENTS; i++)
         {
             Vector2 _VA, _VB, _VC;
@@ -207,10 +246,15 @@ public class UI_Core : MonoBehaviour {
                 _VC = _VECS[i + 1];
 
             GL.Begin(GL.TRIANGLES);
-            Add_VERT(_VA.x + _x, _VA.y + _y, _col);
-            Add_VERT(_VB.x + _x, _VB.y + _y, _col);
-            Add_VERT(_VC.x + _x, _VC.y + _y, _col);
+            Add_VERT_1to1(_VA.x, _VA.y, _col);
+            Add_VERT_1to1(_VB.x, _VB.y, _col);
+            Add_VERT_1to1(_VC.x, _VC.y, _col);
             GL.End();
         }
+        GL.PopMatrix();
+    }
+
+    public static void Draw_CROSS(float _x, float _y, float _thickness, float _size, Color _col, float _rotation = 0){
+        
     }
 }
